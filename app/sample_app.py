726df03d7d8ca45995107fa2d5b806ecfddcde99
Heidy-Vivas <hvivas2208@gmail.com>
2026-08-26 00:00:17 -0500

    test: introducir vulnerabilidades intencionales para demo DevSecOps

--- app/sample_app.py
+++ app/sample_app.py
@@ -1,9 +1,11 @@
+MYSQL_PASSWORD = "super_secret_123"
+
 from flask import Flask
 from flask import request
 from flask import render_template
 sample = Flask (__name__)
 @sample .route ("/")
 def main():
- return render_template ("index.html")
+ return render_template ("index.html"), 500
 if __name__ == "__main__":
- sample.run(host="0.0.0.0", port=5050)  # nosec B104
+ sample.run(host="0.0.0.0", port=5050, debug=True)  # nosec B104
